d8cd41479882a4896229fc47403fc613e7d781f8
Fabiele Hoerlle <84751585+fabielehoerlle@users.noreply.github.com>
2023-01-19 15:29:36 -0300

    Update ProjectItem

--- lib/widgets/ProjectItem.dart
+++ lib/widgets/ProjectItem.dart
@@ -4,48 +4,71 @@ class ProjectItem extends StatelessWidget {
   ProjectItem({
     required this.image,
     required this.title,
+    required this.dialogContent,
+    this.dialogBackgroundColor = Colors.white,
     Key? key,
   }) : super(key: key);
   final String image;
   final String title;
+  final Widget dialogContent;
+  final Color dialogBackgroundColor;
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          height: 200,
-          width: 200,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10),
-            image: DecorationImage(
-              image: AssetImage(image),
-              fit: BoxFit.cover,
+    return GestureDetector(
+      onTap: () {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) => Dialog(
+            backgroundColor: dialogBackgroundColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Container(
+              padding: const EdgeInsets.all(20),
+              height: 500,
+              width: 730,
+              child: dialogContent,
             ),
           ),
-        ),
-        Positioned(
-          bottom: 10,
-          right: 0,
-          child: Container(
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(10),
-                topLeft: Radius.circular(10),
-              ),
-              color: Colors.black,
-            ),
-            width: 150,
-            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-            child: Text(
-              title,
-              style: const TextStyle(
-                color: Color(0xFFEDE9F2),
+        );
+      },
+      child: Stack(
+        children: [
+          Container(
+            height: 200,
+            width: 200,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              image: DecorationImage(
+                image: AssetImage(image),
+                fit: BoxFit.cover,
               ),
             ),
           ),
-        ),
-      ],
+          Positioned(
+            bottom: 10,
+            right: 0,
+            child: Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(10),
+                  topLeft: Radius.circular(10),
+                ),
+                color: Colors.black,
+              ),
+              width: 150,
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              child: Text(
+                title,
+                style: const TextStyle(
+                  color: Color(0xFFEDE9F2),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
